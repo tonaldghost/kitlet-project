8d84850c566a742edf18215c1680fd6b50bca846
Flying Kitlets <aaronjohnstanton@gmail.com>
2019-11-18 16:52:17 +0000

    can loop over state array for Items on discover screen

--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -3,14 +3,35 @@ import { StyleSheet, View, Text } from "react-native";
 
 export default class DiscoverScreen extends React.Component {
   static navigationOptions = {
-    header: null,
-    takePicture: false
+    header: null
   };
-
+  state = {
+    takePicture: false,
+    items: [
+      {
+        title: "1984 Minimoog",
+        img: "aaroniousbosch-1574072891959",
+        isAvailable: true,
+        location: "Guiseley",
+        pricePerDay: 42
+      },
+      {
+        title: "Mic",
+        img: "aaroniousbosch-1574072891959",
+        isAvailable: true,
+        location: "Guiseley",
+        pricePerDay: 42
+      }
+    ]
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Welcome to Kitlet</Text>
+        <Text>Hello</Text>
+        {this.state.items.map((item, index) => {
+          return <Text>{item.title}</Text>;
+        })}
       </View>
     );
   }
@@ -18,5 +39,5 @@ export default class DiscoverScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: { flex: 1, paddingTop: 50, alignItems: "center" },
-  header: { fontSize: 22}
+  header: { fontSize: 22 }
 });
