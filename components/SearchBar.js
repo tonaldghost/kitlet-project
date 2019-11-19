d84f7a4d95e4039e9bf3b7dc7a072958a3d93764
Flying Kitlets <aaronjohnstanton@gmail.com>
2019-11-19 15:57:21 +0000

    can now order by price

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -6,52 +6,131 @@ const width = Dimensions.get("window").width;
 
 export default class SearchBar extends Component {
   state = {
-    searchInput: ""
+    searchInput: "",
+    isSorting: false,
+    priceAsc: false
+  };
+  toggleSortBy = () => {
+    this.setState(currentState => {
+      return { isSorting: !currentState.isSorting };
+    });
   };
   handleInput = e => {
-    if (this.state.searchInput.length > 2) {
-      // passed refefernere as prop
+    if (e.nativeEvent.text.length < this.state.searchInput.length) {
+      this.props.resetResults(),
+        () => {
+          this.props.filterResults();
+        };
+    } else if (this.state.searchInput.length > 2) {
       this.props.filterResults(e.nativeEvent.text);
-      // console.log("woof");
+    } else {
+      this.props.resetResults();
     }
     this.setState({ searchInput: e.nativeEvent.text });
   };
+
+  sortByPrice = () => {
+    // console.log("sorting by price");
+    this.setState({ priceAsc: !this.state.priceAsc }, () => {
+      this.state.priceAsc
+        ? this.props.orderByPrice(true)
+        : this.props.orderByPrice(false);
+    });
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Ionicons name="ios-search" size={26} style={styles.icon} />
-        <TextInput
-          style={styles.searchBox}
-          placeholder="search kitlet..."
-          onChange={this.handleInput}
-          value={this.state.searchInput}
-        ></TextInput>
-      </View>
+      <>
+        <View style={styles.outerContainer}>
+          <View style={styles.container}>
+            <Ionicons name="ios-search" size={26} style={styles.icon} />
+            <TextInput
+              style={styles.searchBox}
+              placeholder="Search Kitlet..."
+              onChange={this.handleInput}
+              value={this.state.searchInput}
+            ></TextInput>
+          </View>
+          <Ionicons
+            name="ios-funnel"
+            size={26}
+            onPress={this.toggleSortBy}
+            style={styles.toggleSorting}
+          />
+        </View>
+        {this.state.isSorting && (
+          <View style={styles.sortBox}>
+            <Text style={styles.sortButtons} onPress={this.sortByPrice}>
+              Price
+            </Text>
+            <Text style={styles.sortButtons}>Location</Text>
+            <Text style={styles.sortButtons}>Availablity</Text>
+          </View>
+        )}
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  outerContainer: {
+    display: "flex",
+    width,
+    alignItems: "center",
+    flexDirection: "row"
+  },
   container: {
     display: "flex",
+    borderRadius: 5,
+    overflow: "hidden",
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 16
+    justifyContent: "center",
+    marginBottom: 16,
+    width: width - 88,
+    borderWidth: 1,
+    borderColor: "#eee",
+    marginLeft: 16,
+    marginRight: 16
+  },
+  sortBox: {
+    display: "flex",
+    borderRadius: 5,
+    overflow: "hidden",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginBottom: 16,
+    width: width - 32,
+    borderWidth: 1,
+    paddingBottom: 16,
+    paddingTop: 16,
+    borderColor: "#eee",
+    marginLeft: 16,
+    marginRight: 16
   },
   searchBox: {
     backgroundColor: "#f0f0f0",
     paddingLeft: 16,
     paddingTop: 8,
     paddingBottom: 8,
-    width,
+    width: width - 88,
     fontSize: 18
   },
   icon: {
     backgroundColor: "#f0f0f0",
-    paddingLeft: 16,
+    paddingLeft: 32,
     paddingTop: 12.5,
     paddingBottom: 12.5,
     fontSize: 18,
     color: "#ddd"
+  },
+  toggleSorting: {
+    backgroundColor: "white",
+    paddingBottom: 12,
+    color: "#333"
+  },
+  sortButtons: {
+    textAlign: "center"
   }
 });
--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -4,67 +4,98 @@ import { StyleSheet, View, Text } from "react-native";
 import SearchBar from "../components/SearchBar";
 import { ScrollView } from "react-native-gesture-handler";
 
+const dummyData = [
+  {
+    title: "1984 Minimoog",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
+    isAvailable: true,
+    location: "Guiseley",
+    price: 10,
+    body:
+      "The Minimoog is a vintage synthesiser from the 80s made by moog. This iconic sound can be heard on many of the 80s biggest tracks"
+  },
+  {
+    title: "1984 Minimoog",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
+    isAvailable: true,
+    location: "Woof",
+    price: 20,
+    body:
+      "The Minimoog is a vintage synthesiser from the 80s made by moog. This iconic sound can be heard on many of the 80s biggest tracks"
+  },
+  {
+    title: "1984 Minimoog",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
+    isAvailable: true,
+    location: "Guiseley",
+    price: 10,
+    body:
+      "The Minimoog is a vintage synthesiser from the 80s made by moog. This iconic sound can be heard on many of the 80s biggest tracks"
+  },
+  {
+    title: "1984 Minimoog",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
+    isAvailable: true,
+    location: "Guiseley",
+    price: 20,
+    body:
+      "The Minimoog is a vintage synthesiser from the 80s made by moog. This iconic sound can be heard on many of the 80s biggest tracks"
+  },
+  {
+    title: "1984 Minimoog",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
+    isAvailable: true,
+    location: "Guiseley",
+    price: 10,
+    body:
+      "The Minimoog is a vintage synthesiser from the 80s made by moog. This iconic sound can be heard on many of the 80s biggest tracks"
+  },
+  {
+    title: "1984 Minimoog",
+    img:
+      "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
+    isAvailable: true,
+    location: "Guiseley",
+    price: 20,
+    body:
+      "The Minimoog is a vintage synthesiser from the 80s made by moog. This iconic sound can be heard on many of the 80s biggest tracks"
+  }
+];
+
 export default class DiscoverScreen extends React.Component {
   static navigationOptions = {
     header: null
   };
   state = {
     takePicture: false,
-    items: [
-      {
-        title: "1984 Minimoog",
-        img:
-          "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
-        isAvailable: true,
-        location: "Guiseley",
-        price: 10
-      },
-      {
-        title: "1984 Minimoog",
-        img:
-          "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
-        isAvailable: true,
-        location: "Guiseley",
-        price: 20
-      },
-      {
-        title: "1984 Minimoog",
-        img:
-          "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
-        isAvailable: true,
-        location: "Guiseley",
-        price: 10
-      },
-      {
-        title: "1984 Minimoog",
-        img:
-          "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
-        isAvailable: true,
-        location: "Guiseley",
-        price: 20
-      },
-      {
-        title: "1984 Minimoog",
-        img:
-          "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
-        isAvailable: true,
-        location: "Guiseley",
-        price: 10
-      },
-      {
-        title: "1984 Minimoog",
-        img:
-          "https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b",
-        isAvailable: true,
-        location: "Guiseley",
-        price: 20
-      }
-    ]
+    items: dummyData
+  };
+  orderByPrice = ascending => {
+    console.log("ordering by price function", ascending);
+    this.setState(currentState => {
+      const itemsClone = [...currentState.items];
+      const sortedItems = itemsClone.sort((a, b) => {
+        return ascending ? a.price - b.price : b.price - a.price;
+      });
+      return { items: sortedItems };
+    });
+  };
+  resetResults = () => {
+    this.setState({ items: dummyData });
   };
   filterResults = searchTerm => {
     const itemsClone = [...this.state.items];
     const filteredItems = itemsClone.filter(item => {
-      return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+      return (
+        item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.body.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.location.toLowerCase().includes(searchTerm.toLowerCase())
+      );
     });
     this.setState({ items: filteredItems });
   };
@@ -72,7 +103,11 @@ export default class DiscoverScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Welcome to Kitlet</Text>
-        <SearchBar filterResults={this.filterResults} />
+        <SearchBar
+          orderByPrice={this.orderByPrice}
+          filterResults={this.filterResults}
+          resetResults={this.resetResults}
+        />
         <ScrollView>
           {this.state.items.map((item, index) => {
             return <ItemCard key={index} props={item} />;
