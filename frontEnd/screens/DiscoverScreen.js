c743db2f678876f43341517a7c40dc0e81285681
Flying Kitlets <aaronjohnstanton@gmail.com>
2019-11-20 12:14:41 +0000

    styled IndividualItem component

--- frontEnd/screens/DiscoverScreen.js
+++ frontEnd/screens/DiscoverScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ItemCard from "../components/ItemCard";
-import { StyleSheet, View, Text, TouchableOpacity, Image, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Button,
+  Text,
+  TouchableOpacity,
+  Image,
+  Dimensions
+} from "react-native";
 import SearchBar from "../components/SearchBar";
 import { ScrollView } from "react-native-gesture-handler";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import mainGreen from "../constants/Colors";
+import tintColor from "../constants/Colors";
+
+import Icon from "react-native-vector-icons/EvilIcons";
+const myIcon = <Icon name="location" size={30} color={tintColor.tintColor} />;
 
 const dummyData = [
   {
@@ -134,10 +147,49 @@ class IndividualItemScreen extends React.Component {
     const width = Dimensions.get("window").width;
     const styles = StyleSheet.create({
       container: { paddingTop: 50, paddingBottom: 84 },
-      itemCardImage: { flex: 1, width, height: 64, margin: "auto" },
-      header: { fontSize: 22 }
+      itemCardImage: { flex: 1, width, height: 64, margin: 32 },
+      innerContent: { flex: 2, width, paddingLeft: 16, paddingRight: 16 },
+      fixedIsAvailable: {
+        position: "absolute",
+        right: 16,
+        color: mainGreen.mainGreen,
+        fontSize: 22
+      },
+      price: {
+        fontSize: 22,
+        marginTop: 16,
+        fontWeight: "800",
+        color: mainGreen.mainGreen
+      },
+      perDay: {
+        fontSize: 22,
+        marginTop: 16,
+        fontWeight: "400",
+        color: "black"
+      },
+      location: {
+        marginTop: 16,
+        marginBottom: 16,
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "row",
+        marginLeft: -8
+      },
+      request: {
+        backgroundColor: "#333",
+        paddingBottom: 32
+      },
+      title: { fontSize: 32 },
+      header: { fontSize: 22 },
+      buttonHolder: {
+        marginBottom: 32,
+        display: "flex",
+        width: width - 32,
+        justifyContent: "center"
+      }
     });
     const itemProps = this.props.navigation.state.params;
+
     return (
       <View
         style={{
@@ -150,16 +202,25 @@ class IndividualItemScreen extends React.Component {
       >
         <Image style={styles.itemCardImage} source={{ uri: itemProps.img }} />
         <View style={styles.innerContent}>
-          {itemProps.isAvailable && (
-            <Text style={styles.fixedIsAvailable}>Available</Text>
-          )}
           <Text style={styles.title}>{itemProps.title}</Text>
-          <Text style={styles.location}>{itemProps.location}</Text>
+
+          <View style={styles.location}>
+            {myIcon}
+            <Text>{itemProps.location}</Text>
+          </View>
+          <Text style={styles.body}>{itemProps.body}</Text>
+
           <Text style={styles.price}>
             £{itemProps.price}
             <Text style={styles.perDay}>/day</Text>
           </Text>
-          <Text>{itemProps.title}</Text>
+        </View>
+        <View style={styles.buttonHolder}>
+          <Button
+            title="Request Item"
+            style={styles.request}
+            color={tintColor.tintColor}
+          />
         </View>
       </View>
     );
@@ -175,6 +236,10 @@ const DiscoverScreenNavigation = createStackNavigator(
     initialRouteName: "Discover"
   }
 );
+
+IndividualItemScreen.navigationOptions = {
+  title: "Back To More Items"
+};
 
 const DicoverContainer = createAppContainer(DiscoverScreenNavigation);
 
