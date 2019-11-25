8b9d6479e5060fdf89ebf97c28db4b7cfbd9047c
Umayr Shaffi <ushaffi95@gmail.com>
2019-11-25 14:53:48 +0000

    Heroku push commit

--- backEnd/db/data/dev-data/items.js
+++ backEnd/db/data/dev-data/items.js
@@ -3,7 +3,7 @@ module.exports = [
 		title: 'MIDI Keyboard',
 		owner: 'umayrs95',
 		body: 'MIDI keyboard for music production',
-		category: 'Music',
+		category: 'music',
 		img_url:
 			'https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Fumayrs95-1574072432159?alt=media&token=d304da10-8271-4c7b-803c-bee8307a254b',
 		is_available: true,
@@ -15,7 +15,7 @@ module.exports = [
 		title: 'Art Drawing Tablet',
 		owner: 'umayrs95',
 		body: 'Drawing tablet in good condtition, slight ding in corner',
-		category: 'Art',
+		category: 'art',
 		img_url:
 			'https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Fumayrs95-1574072424794?alt=media&token=94a065e2-bfbb-41c3-add4-0e2a9fbc82b2',
 		is_available: true,
@@ -27,7 +27,7 @@ module.exports = [
 		title: 'Boom Mic',
 		owner: 'aaroniousbosch',
 		body: 'Boom microphone in good condition, slight wear on the boom pole',
-		category: 'Music',
+		category: 'music',
 		img_url:
 			'https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072895305?alt=media&token=165216b8-eaeb-4415-81b9-af0b2264d028',
 		is_available: true,
@@ -39,7 +39,7 @@ module.exports = [
 		title: 'Minimoog Synth',
 		owner: 'aaroniousbosch',
 		body: 'Minimoog synth, in great condition and sounds amazing',
-		category: 'Music',
+		category: 'music',
 		img_url:
 			'https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Faaroniousbosch-1574072891959?alt=media&token=834bfaae-9f63-4cd9-af1d-47e506cf630b',
 		is_available: true,
@@ -50,7 +50,7 @@ module.exports = [
 		title: 'Wireless Mic and Receiver',
 		owner: 'tonyboi',
 		body: 'Wireless mic with receiver, batteries not included',
-		category: 'Audio',
+		category: 'audio',
 		img_url:
 			'https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Ftonyboi-1574072795955?alt=media&token=e9a9ce56-7ed2-444f-a081-dcd6ffe617dc',
 		is_available: true,
@@ -62,7 +62,7 @@ module.exports = [
 		title: 'GoPro Camera',
 		owner: 'tonyboi',
 		body: 'GoPro camera, dont my nails look great in the pic',
-		category: 'Audio',
+		category: 'audio',
 		img_url:
 			'https://firebasestorage.googleapis.com/v0/b/kitlet-784db.appspot.com/o/images%2Ftonyboi-1574072799672?alt=media&token=8a0b4342-4b9e-4a16-aba4-530694468006',
 		is_available: true,
